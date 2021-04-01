2c442049595480a104063caaf40ad6a27ea60143
Johannes Flitta <johannes@flitta.de>
2021-04-01 12:21:02 +0300

    added basic search strategy

---
+++ src/komm-rein.api/Services/ISearchService.cs
@@ -0,0 +1,19 @@
+﻿using komm_rein.model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace komm_rein.api.Services
+{
+    public interface ISearchService<T>
+    {
+        ValueTask<List<T>> Search(string search, int? take, int? skip);
+
+        ValueTask<List<T>> Search(string search);
+
+        Task UpsertIntoIndex(T item);
+    }
+}
